12229589cc3f6eaf7666e4315ee8af79613c431d
bigalr8 <bigalr8@icloud.com>
2019-04-21 08:59:15 -0400

    added external node package modules to code

--- liri.js
+++ liri.js
@@ -4,14 +4,22 @@
 //Include external modules
 //Axios Promise based HTTP client for the browser and node.JS
 var axios = require("axios");
-//dotenv
-//keys.js
-//node-spotify
-//Create spotify client
-//moment
-//fs 
+//dotenv loads enviroment variables from a .env file into the process.env
+//This stores configuration and access code inforation separate from code
+//It will be used in this app to conceal Spotify credentials
+var dotenv = require("dotenv");
+//keys.js exports the Spotify credentials from the environment variables loaded for them by "dotenv"
+var keys = require("./keys.js");
+//node-spotify-api is the API liobrary for the Spotify REST API
+var Spotify = require ("node-spotify-api");
+//moment module will be used to format display dates and times
+//fs will be used for file system processing (.e. reading and writing to files)
+
+//Create spotify client with the required credentials 
+var spotify = new Spotify(keys.spotify);
 
 //Variables 
+
 //Node arguments
 //feature operators
 //concert-this
